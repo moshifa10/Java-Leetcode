9e629c4351d91004b6eb9ab66b73e670993d48c4
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-04-22 22:06:04 +0200

    added the code and it could solve but not all problems working to enhance it

---
+++ src/main/java/practice/majozi/RomanNumeralsEncoder.java
@@ -0,0 +1,45 @@
+package practice.majozi;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class RomanNumeralsEncoder {
+
+    public static void main(String[] args){
+        RomanNumeralsEncoder check = new RomanNumeralsEncoder();
+
+        System.out.println(check.solution(1));
+        System.out.println(check.solution(4));
+        System.out.println(check.solution(3));
+        System.out.println(check.solution(6));
+    }
+
+    public String solution(int n) {
+
+        ArrayList<String> numerals = new ArrayList<>();
+        String[] romanNumerals = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+        int[] numbers = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+
+        for (int i=romanNumerals.length-1; i > -1; i--){
+
+            // Expression
+            int floor =  n / numbers[i];
+
+            if (floor > 0){
+                n -= (n * floor);
+                if (floor > 1){
+                    for(int j = 0; j<floor; j++){
+                        numerals.add(romanNumerals[i]);
+                    }
+                }
+                else {
+                    numerals.add(romanNumerals[i]);
+                }
+//                if (n == 0){
+//                    return String.join("",numerals);
+//                }
+            }
+        }
+        return String.join("",numerals);
+    }
+}
