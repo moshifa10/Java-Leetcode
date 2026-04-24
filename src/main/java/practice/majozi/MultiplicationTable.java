91ae7af1cbfdeafb7db5b749571b91b5b6137c5a
Njabulo Moshifa <njmosjhb025@student.wethinkcode.co.za>
2026-04-24 10:54:24 +0200

    completed the multiplication table

---
+++ src/main/java/practice/majozi/MultiplicationTable.java
@@ -0,0 +1,28 @@
+package practice.majozi;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MultiplicationTable {
+
+//    [[1,2,3],[2,4,6],[3,6,9]] = 1
+
+    public static void main(String[] args){
+
+        System.out.println(Arrays.deepToString(MultiplicationTable.multiplicationTable(3)));
+
+    }
+
+    public static int [][] multiplicationTable(int n){
+        int[][] table = new int[n][n];
+
+        for (int i = 1; i <= n; i++){
+            int[] save = new int[n];
+            for (int j = 1; j<n+1; j++){
+                save[j-1] = i*j;
+            }
+            table[i-1] = save;
+        }
+        return table;
+    }
+}
